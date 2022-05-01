f0b8fcf77dc6e031474a9f31c6cfc543cd987b39
012night <71623834+012night@users.noreply.github.com>
2022-05-01 14:10:38 +0900

    Add files via upload

---
+++ Day01/lecture04.py
@@ -0,0 +1,41 @@
+#1.문자열 자료형은 "" '' """ ''' 의 형태
+
+# a = "Hello World"
+# print(a)
+#Python's fun은 어떻게 출력?
+# a = "python's fun"
+# print(a)
+
+#2.여러줄로 이루어진 문자열 : \n
+# a = "Life is too short, \nYou need python "
+# print(a)
+#더 편한 방법
+# a = """
+# Life is too short,
+#
+# test
+#
+# You need python """
+# print(a)
+
+
+#3.문자열 자료형은 str
+# print(type(a))
+
+#4.문자열 더하고 곱하기
+# a = "Python "
+# b = "is fun"
+# print(a+b)
+
+#5.문자열 인덱싱, 모든 인덱싱은 0부터 시작
+# a = "Python is fun"
+# print(a[-1])
+
+#6.문자열 슬라이싱, [x:y] x이상 y미만
+# a = "Python is fun"
+# print(a[:6])
+# print(a[6:])
+# print(a[::3])
+# print(a[::-1])
+
+print(1 + 2)
