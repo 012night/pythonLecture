e5d979a7010698d136a346651047e6ef53c481cb
012night <71623834+012night@users.noreply.github.com>
2022-07-10 22:44:44 +0900

    Add files via upload

---
+++ Day02/lecture02_6.py
@@ -0,0 +1,32 @@
+#형변환
+# stra = "Hello"
+# inta = 5
+# print(stra + inta)
+#int >> str 로 갈땐 str()
+#str >> int 로 갈땐 int()
+
+
+# #연습문제 1.
+# print("입력값 숫자", 4)
+# print("입력값 숫자 여러개", 4, 5, 6)
+# su =5
+# dan = 800
+# print("su 주소 : " + str(id(su)))
+# print("dan 주소 : " + str(id(dan)))
+# print("금액 : " + str(su*dan))
+# print("금액 :", su*dan)
+
+
+#연습문제 3.
+#사용자입력
+# num = input()
+# print(num)
+# num = input("숫자를 입력하세요")
+# print(num)
+# print(type(num))
+# #파이썬에서는 항상 문자열로 입력을 받는다.
+# fat = input("지방의 그램을 입력하세요 ")
+# tan = input("탄수화물의 그램을 입력하세요 ")
+# dan = input("단백질의 그램을 입력하세요 ")
+# total = int(fat)*9 + int(dan)*4 + int(tan)*4
+# print("총칼로리 :", total)
