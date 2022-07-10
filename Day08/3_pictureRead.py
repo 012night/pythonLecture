88d3f59d6fefac09687eb816db7ca3eacb12e601
012night <71623834+012night@users.noreply.github.com>
2022-07-10 22:44:52 +0900

    Delete Day08 directory

--- Day08/3_pictureRead.py
+++
@@ -1,7 +0,0 @@
-#이번엔 그림파일을 불러오기/저장하기!
-#그림파일은 읽어올때 r모드만 써서는 안된다! 그림은 바이너리 파일이기에 바이너리 파일로 불러와라 라고 해줘야 해
-# my_image = open("C:\chicoding\pic_test.jpg","rb")
-# my_image_copy = open("C:\chicoding\pic_test2.jpg","wb")
-# my_image_copy.write(my_image.read())
-# my_image.close()
-# my_image_copy.close()
