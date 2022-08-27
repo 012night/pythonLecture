9177fb98f0d229210205ce73bafbffa0b4299b88
012night <71623834+012night@users.noreply.github.com>
2022-08-27 19:19:44 +0900

    Add files via upload

---
+++ Day05/lecture05_algorism.py
@@ -0,0 +1,22 @@
+# ========================================================================
+# # 알고리즘
+# # 최대값 최소값 을 for문을 이용하여 구하라    
+# import random
+# num_list = []
+# initNum = 0
+# while initNum < 10:
+#     num_list.append(random.randint(1, 100))
+#     initNum += 1
+# print(num_list)
+# # lst = [93, 45, 21, 30, 20, 94, 66, 71, 45]
+# max = num_list[0]
+# min = num_list[0]
+
+# for i in num_list:
+#     if max < i:
+#         max = i
+#     if min > i:
+#         min = i
+# print("최대값 : ", max)
+# print("최소값 : ", min)
+# ========================================================================
