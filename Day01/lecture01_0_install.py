2aef728ac644f69b596cd9e1a77cf908b3327718
012night <71623834+012night@users.noreply.github.com>
2022-09-17 23:42:35 +0900

    Add files via upload

--- Day01/lecture01_0_install.py
+++ Day01/lecture01_0_install.py
@@ -5,28 +5,44 @@
 
  
 # 1.파이썬 설치
+
 #  1)구글에 python download 검색 첫 줄 클릭(www.python.org)
+
 #  2)Download Python 클릭
+
 #  3)다운로드 폴더에 저장된 설치파일을 실행
+
 #  4)add python path 체크후에 Customize installation에 들어가 나머지 옵션 모두 클릭
+
 #  5)install
 
  
 # 2.vs code 설치
 #   1)구글에 vscode 검색 첫 줄 클릭(code.visualstudio.com)
+
 #   2)다운로드 버튼 클릭
+
 #   3)체크박스 모두 선택 후 설치 진행
+
 #   4)VScode 실행 후 Extention 클릭 후 python 검색(첫줄 나오는것 다운)
+
 #   5) explorer 클릭
+
 
 
 # 3.파이참 설치
 #  1)구글에 pycharm download 검색 첫 줄 클릭
+
 #  2)Community 버전 다운로드
+
 #  3)다운로드 폴더에 저장된 설치파일을 실행
+
 #  4)default 옵션으로 설치 진행
+
 #  5)do not import 버튼 클릭 후 실행
+
 #  6)NewProject 클릭 후 프로젝트 생성
+
 #  7)default 설정으로 생성되어 열린 main.py 파일에서 오른쪽 상단에 재생버튼을 run 해보자. Hi, PyCharm 이라는 내용이 하단의 run탭에서 정상적으로 출력되고 있는 것을 확인할 수 있을 것이다.
 
  # 이제 파이썬이 정상 설치되었고, 개발환경 준비가 끝났다.
