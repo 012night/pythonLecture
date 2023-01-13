663347061520b7dd99cad204e477913e55616a13
012night <71623834+012night@users.noreply.github.com>
2023-01-14 08:43:09 +0900

    Delete Day08 directory

--- Day08/employee_parent.py
+++
@@ -1,9 +0,0 @@
-class Employee :
-    def __init__(self, name, emptype) :
-        self.name = name
-        self.emptype = emptype
-        
-    def print_profile(self) :
-        print("="*30)
-        print("이름 : {}".format(self.name))
-        print("고용형태 : {}".format(self.emptype))
