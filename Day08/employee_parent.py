25ba44144d3d3b32e1e8820835ae2ac4a01e1ad2
012night <71623834+012night@users.noreply.github.com>
2024-02-04 14:25:15 +0900

    Add files via upload

--- Day08/employee_parent.py
+++ Day08/employee_parent.py
@@ -1,13 +1,9 @@
-# 해당 클래스는 이름(name), 고용타입(emptype)을 매개변수로 받아 초기화 하며 print_profile이라는 메서드로 이름, 고용형태를 그대로 출력해준다.
 class Employee :
     def __init__(self, name, emptype) :
         self.name = name
         self.emptype = emptype
-
+        
     def print_profile(self) :
-        print(f"이름 : {self.name}")
-        print(f"고용형태 : {self.emptype}")
-
-if __name__ == "__main__" :
-    e1 = Employee("김메가", "비정규직")
-    e1.print_profile()
+        print("="*30)
+        print("이름 : {}".format(self.name))
+        print("고용형태 : {}".format(self.emptype))
